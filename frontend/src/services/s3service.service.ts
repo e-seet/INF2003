3eba71879cb2e78a1353d7c31b32d7354a0d834b
2202210 <2202210@sit.singaporetech.edu.sg>
2024-12-01 10:07:59 +0800

    added AWS cloudfront

--- frontend/src/services/s3service.service.ts
+++ frontend/src/services/s3service.service.ts
@@ -8,6 +8,7 @@ import { CognitoIdentityClient } from "@aws-sdk/client-cognito-identity";
 import { fromCognitoIdentityPool } from "@aws-sdk/credential-provider-cognito-identity";
 import { v4 as uuidv4 } from "uuid";
 import { environment } from "./environment";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 @Injectable({
   providedIn: "root",
@@ -15,8 +16,8 @@ import { environment } from "./environment";
 export class S3serviceService {
   private bucketName = "inf2003bucket";
   private region = "us-east-1";
-  private identityPoolId = environment.AWSCognitoIdentity.split("=")[1];
-
+  //   private identityPoolId = environment.AWSCognitoIdentity.split("=")[1];
+  private identityPoolId = "us-east-1:8bb4dddb-b4b8-449f-9b96-b748e4030d91";
   private s3: S3Client;
 
   constructor() {
@@ -30,32 +31,90 @@ export class S3serviceService {
     });
   }
 
-  // Upload file to S3
-  async uploadFile(file: File): Promise<{ fileId: string; location: string }> {
-    const fileId = uuidv4(); // Generate a unique file ID
-
-    console.log("upload file");
-    console.log(this.identityPoolId);
-
-    // Configure the S3 upload parameters
-    let params = {
-      Bucket: this.bucketName,
-      Key: `${fileId}-${file.name}`, // Use fileId and filename to ensure uniqueness
-      Body: file,
-      ACL: "public-read" as ObjectCannedACL, // Optional: Make the file publicly accessible
-    };
+  // Generate a signed URL for uploading files to S3
+  async getSignedUrl(file: File, fileId: string): Promise<string> {
+    const key = `${fileId}-${file.name}`; // Key for the object
 
     try {
-      // Use the PutObjectCommand to upload the file
-      const data = await this.s3.send(new PutObjectCommand(params));
+      const command = new PutObjectCommand({
+        Bucket: this.bucketName,
+        Key: key,
+        ContentType: file.type, // MIME type of the file
+      });
 
+      // Generate a signed URL using the presigner
+      const signedUrl = await getSignedUrl(this.s3, command, {
+        expiresIn: 3600,
+      }); // URL valid for 1 hour
+      console.log("Generated signed URL:", signedUrl);
+      return signedUrl;
+    } catch (error) {
+      console.error("Error generating signed URL:", error);
+      throw error;
+    }
+  }
+
+  // Upload file to S3 using the signed URL
+  async uploadFile(file: File): Promise<{ fileId: string; location: string }> {
+    const fileId = uuidv4(); // Generate a unique file ID
+    console.log("fileid");
+    console.log(fileId);
+    const signedUrl = await this.getSignedUrl(file, fileId);
+
+    try {
+      // Upload the file using the signed URL
+      const response = await fetch(signedUrl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": file.type, // Set the correct content type
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to upload file. Status: ${response.status}`);
+      }
+
+      console.log("File uploaded successfully");
+
+      const fileId = signedUrl.split("?")[0].split("/").pop(); // Extract file ID from URL
+      console.log("file id" + fileId);
       return {
-        fileId: fileId, // Return the generated fileId
-        location: `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${params.Key}`, // The location of the uploaded file
+        fileId: fileId || "", // Return the generated fileId
+        location: "https://d1ex7sr8l8poxt.cloudfront.net/" + fileId, // Return the location of the uploaded file
       };
     } catch (error) {
       console.error("Error uploading file:", error);
       throw error;
     }
   }
+
+  // Upload file to S3
+  //   async uploadFile(file: File): Promise<{ fileId: string; location: string }> {
+  //     const fileId = uuidv4(); // Generate a unique file ID
+
+  //     console.log("upload file");
+  //     console.log(this.identityPoolId);
+
+  //     // Configure the S3 upload parameters
+  //     let params = {
+  //       Bucket: this.bucketName,
+  //       Key: `${fileId}-${file.name}`, // Use fileId and filename to ensure uniqueness
+  //       Body: file,
+  //       ACL: "public-read" as ObjectCannedACL, // Optional: Make the file publicly accessible
+  //     };
+
+  //     try {
+  //       // Use the PutObjectCommand to upload the file
+  //       const data = await this.s3.send(new PutObjectCommand(params));
+
+  //       return {
+  //         fileId: fileId, // Return the generated fileId
+  //         location: `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${params.Key}`, // The location of the uploaded file
+  //       };
+  //     } catch (error) {
+  //       console.error("Error uploading file:", error);
+  //       throw error;
+  //     }
+  //   }
 }
